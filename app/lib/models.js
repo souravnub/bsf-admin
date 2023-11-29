c0724351a1afd3fa7be57101a7dca05010b5c8fc
Sourav <s6hr6h4u3@gmail.com>
2023-11-29 13:12:17 -0700

    feat: made contact model

--- app/lib/models.js
+++ app/lib/models.js
@@ -105,6 +105,34 @@ const courseSchema = new mongoose.Schema(
     { timestamps: true }
 );
 
+const contactSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: [true, "Please provide first name"],
+        },
+        lastName: {
+            type: String,
+            required: [true, "Please provide last name"],
+        },
+        interestCategories: [{ type: String }],
+        email: {
+            type: String,
+            required: [true, "Please provide email"],
+            match: [
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                "Please provide a valid email",
+            ],
+        },
+        message: {
+            type: String,
+            trim: true,
+            minLength: [10, "message should be atleast 10 characters"],
+        },
+    },
+    { timestamps: true }
+);
+
 export const Admin =
     mongoose.models.Admin || mongoose.model("Admin", adminSchema);
 
@@ -117,3 +145,6 @@ export const Course =
 export const CourseCategory =
     mongoose.models.CourseCategory ||
     mongoose.model("CourseCategory", courseCategorySchema);
+
+export const Contact =
+    mongoose.models.Contact || mongoose.model("Contact", contactSchema);
