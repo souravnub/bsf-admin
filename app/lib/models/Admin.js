ba819bb9b61b1c8c0d43bfa50e19f1e69fd41165
Sourav <s6hr6h4u3@gmail.com>
2023-11-30 10:28:26 -0700

    fix: moved models into individual files

---
+++ app/lib/models/Admin.js
@@ -0,0 +1,25 @@
+const { default: mongoose } = require("mongoose");
+
+const adminSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            min: 3,
+            max: 20,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        isAdmin: {
+            type: Boolean,
+            default: true,
+        },
+    },
+    { timestamps: true }
+);
+
+export const Admin =
+    mongoose.models.Admin || mongoose.model("Admin", adminSchema);
---
+++ app/lib/models/Contact.js
@@ -0,0 +1,32 @@
+const { default: mongoose } = require("mongoose");
+
+const contactSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: [true, "Please provide first name"],
+        },
+        lastName: {
+            type: String,
+            required: [true, "Please provide last name"],
+        },
+        interestCategories: [{ type: String }],
+        email: {
+            type: String,
+            required: [true, "Please provide email"],
+            match: [
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                "Please provide a valid email",
+            ],
+        },
+        message: {
+            type: String,
+            trim: true,
+            minLength: [10, "message should be atleast 10 characters"],
+        },
+    },
+    { timestamps: true }
+);
+
+export const Contact =
+    mongoose.models.Contact || mongoose.model("Contact", contactSchema);
