075f22e90c69d99ff0c91b89faf06540ea82dcd7
Sourav <s6hr6h4u3@gmail.com>
2023-12-21 16:19:56 -0700

    feat: admin can add and remove videoURLs & categories for watch demo modal

--- app/ui/dashboard/website-content/watch-demo/WatchDemo.jsx
+++ app/ui/dashboard/website-content/watch-demo/WatchDemo.jsx
@@ -5,21 +5,25 @@ import {
     addVideoGalleryCategory,
     deleteUrlFromGallery,
     deleteVideoGalleryCategory,
+    fetchVideoGalleryTabs,
 } from "@/app/lib/actions";
 import { Button } from "../Button";
 import { TiDelete } from "react-icons/ti";
 
-function WatchDemo({ tabs, allUrls }) {
+const WatchDemo = async () => {
+    const tabs = await fetchVideoGalleryTabs();
+
     return (
         <div className={styles.container}>
             <h3 className={styles.sectionHeading}>
                 Manage videos in the demo modal
             </h3>
+
             <AddForm
                 action={addVideoGalleryCategory}
-                inputPlaceholder='New Category'
-                inputId='addCategory'
-                inputLabel='New Category'
+                inputPlaceholder="New Category"
+                inputId="addCategory"
+                inputLabel="New Category"
                 className={styles.form}
             />
             {tabs &&
@@ -27,46 +31,45 @@ function WatchDemo({ tabs, allUrls }) {
                 tabs.map(({ _id, category, url }) => (
                     <div key={_id} className={styles.tabContainer}>
                         <div>
-                            <h4>{category}</h4>
+                            <h2>{category}</h2>
                             <Button
                                 className={`${styles.button} ${styles.removeButton}`}
                                 onClick={deleteVideoGalleryCategory.bind(
                                     null,
                                     _id
-                                )}
-                            >
+                                )}>
                                 Remove Category
                             </Button>
                         </div>
-                        <ol>
-                            {url.map((url) => (
-                                <li key={url}>
-                                    <span>{url}</span>
-                                    <Button
-                                        className={`${styles.deleteButton}`}
-                                        onClick={deleteUrlFromGallery.bind(
-                                            null,
-                                            _id,
-                                            url
-                                        )}
-                                    >
-                                        <TiDelete />
-                                    </Button>
-                                </li>
-                            ))}
-                        </ol>
+                        {url.length > 0 && (
+                            <ol>
+                                {url?.map((url) => (
+                                    <li key={url}>
+                                        <span>{url}</span>
+                                        <Button
+                                            className={`${styles.deleteButton}`}
+                                            onClick={deleteUrlFromGallery.bind(
+                                                null,
+                                                _id,
+                                                url
+                                            )}>
+                                            <TiDelete />
+                                        </Button>
+                                    </li>
+                                ))}
+                            </ol>
+                        )}
                         <AddForm
-                            allUrls={allUrls}
                             extraProps={{ _id }}
                             action={addUrlToGallery}
-                            inputId='urlInput'
-                            inputPlaceholder='Add url'
+                            inputId="urlInput"
+                            inputPlaceholder="Add url"
                             className={styles.form}
                         />
                     </div>
                 ))}
         </div>
     );
-}
+};
 
 export default WatchDemo;
