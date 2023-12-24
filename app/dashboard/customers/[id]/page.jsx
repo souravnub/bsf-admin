57eaeb039505e1ec9c86cef094e59f9eda88a623
Zaid Ahmad <zaidd250@gmail.com>
2023-12-23 21:32:34 -0700

    remove uncessary import

--- app/dashboard/customers/[id]/page.jsx
+++ app/dashboard/customers/[id]/page.jsx
@@ -1,5 +1,5 @@
 import { updateUser } from "@/app/lib/actions";
-import { fetchCustomer, fetchUser } from "@/app/lib/data";
+import { fetchCustomer } from "@/app/lib/data";
 import styles from "@/app/ui/dashboard/users/singleUser/singleUser.module.css";
 import Image from "next/image";
 
@@ -11,33 +11,33 @@ const SingleCustomerPage = async ({ params }) => {
         <div className={styles.container}>
             <div className={styles.infoContainer}>
                 <div className={styles.imgContainer}>
-                    <Image src={user.img || "/noavatar.png"} alt='' fill />
+                    <Image src={user.img || "/noavatar.png"} alt="" fill />
                 </div>
                 {user.username}
             </div>
             <div className={styles.formContainer}>
                 <form action={updateUser} className={styles.form}>
-                    <input type='hidden' name='id' value={user.id} />
+                    <input type="hidden" name="id" value={user.id} />
                     <label>Username</label>
                     <input
-                        type='text'
-                        name='username'
+                        type="text"
+                        name="username"
                         placeholder={user.username}
                     />
                     <label>Email</label>
-                    <input type='email' name='email' placeholder={user.email} />
+                    <input type="email" name="email" placeholder={user.email} />
                     <label>Password</label>
-                    <input type='password' name='password' />
+                    <input type="password" name="password" />
                     <label>Phone</label>
-                    <input type='text' name='phone' placeholder={user.phone} />
+                    <input type="text" name="phone" placeholder={user.phone} />
                     <label>Address</label>
                     <textarea
-                        type='text'
-                        name='address'
+                        type="text"
+                        name="address"
                         placeholder={user.address}
                     />
                     <label>Is Admin?</label>
-                    <select name='isAdmin' id='isAdmin'>
+                    <select name="isAdmin" id="isAdmin">
                         <option value={true} selected={user.isAdmin}>
                             Yes
                         </option>
@@ -46,7 +46,7 @@ const SingleCustomerPage = async ({ params }) => {
                         </option>
                     </select>
                     <label>Is Active?</label>
-                    <select name='isActive' id='isActive'>
+                    <select name="isActive" id="isActive">
                         <option value={true} selected={user.isActive}>
                             Yes
                         </option>
