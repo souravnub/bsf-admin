3fd2b951ad0eb1eb7e5551ec3605a2b5bd700517
Sourav <s6hr6h4u3@gmail.com>
2024-01-09 11:57:07 -0700

    feat: make Instructor Schema

---
+++ app/lib/models/Instructors.js
@@ -0,0 +1,20 @@
+const { default: mongoose } = require("mongoose");
+
+const InstructorSchema = mongoose.Schema({
+    imgUrl: {
+        type: String,
+        default: "https://bsf-web-bucket.s3.us-east-2.amazonaws.com/avatar.png",
+    },
+    name: { type: String, required: true },
+    role: { type: String, required: true },
+    socials: [
+        {
+            name: { type: String, required: true },
+            href: { type: String, required: true },
+        },
+    ],
+});
+
+export const Instructor =
+    mongoose.models.Instructor ||
+    mongoose.model("Instructor", InstructorSchema);
