c9e3c5723ad8cab71243a56bcd1b024635228b70
Zaid Ahmad <zaidd250@gmail.com>
2024-01-10 20:38:11 -0700

    create route for adding a new customer

---
+++ app/api/customers/new/route.js
@@ -0,0 +1,70 @@
+import { Customer } from "@/app/lib/models/Customer";
+import { connectToDB } from "@/app/lib/utils";
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+    try {
+        await connectToDB();
+
+        const body = await request.json();
+
+        const customerExists = await Customer.findOne({ email: body.email });
+
+        if (customerExists) {
+            const currentDate = new Date();
+            const courseEnrolled = body.course;
+
+            customerExists.courses.push({
+                course: courseEnrolled,
+                purchaseDate: currentDate,
+            });
+
+            // Save the modified customer instance
+            await customerExists.save();
+            return NextResponse.json({ message: "Success." });
+        } else {
+            const customerCourses = [];
+            const currentDate = new Date();
+
+            customerCourses.push({
+                course: body.course,
+                purchaseDate: currentDate,
+            });
+
+            const customerData = {
+                email: body.email,
+                name: body.name,
+                courses: customerCourses,
+            };
+
+            const newCustomer = new Customer(customerData);
+            await newCustomer.save();
+
+            return NextResponse.json({ message: "Success." });
+        }
+    } catch (err) {
+        return NextResponse.json(
+            {
+                errorMessage: "Error creating customer",
+                error: err.message,
+            },
+            { status: 500 }
+        );
+    }
+}
+
+export async function OPTIONS(request) {
+    const allowedOrigin = request.headers.get("origin");
+    const response = new NextResponse(null, {
+        status: 200,
+        headers: {
+            "Access-Control-Allow-Origin": allowedOrigin || "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers":
+                "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version",
+            "Access-Control-Max-Age": "86400",
+        },
+    });
+
+    return response;
+}
