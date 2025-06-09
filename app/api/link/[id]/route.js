42ae223dbe54772a97219dda1c96f48b50e86d52
Sourav Kumar <s6hr6h4u3@gmail.com>
2025-06-08 21:23:10 -0600

    change response type for getting the course access link

--- app/api/link/[id]/route.js
+++ app/api/link/[id]/route.js
@@ -8,11 +8,12 @@ export async function GET(request) {
         // getting the id from the url
         const id = request.url.slice(request.url.lastIndexOf("/") + 1);
 
-        const course = await Course.findById(id);
+        const { email_link, name, schedule } = await Course.findById(id);
 
         return NextResponse.json({
-            course_name: course.name,
-            link: course.email_link,
+            accessLink: email_link,
+            name,
+            schedule,
         });
     } catch (err) {
         return NextResponse.json(
